56a2a4bcd0dc5ae707fb102fab58cbcc4d4fb0e9
Romeu Franzoia Junior <rfranzoia@gmail.com>
2023-09-26 14:53:28 +0100

    added api gateway and eureka discovery server

---
+++ stock-api-gateway/src/main/java/com/franzoia/apigateway/StockApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.franzoia.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockApiGatewayApplication.class, args);
+    }
+
+}
